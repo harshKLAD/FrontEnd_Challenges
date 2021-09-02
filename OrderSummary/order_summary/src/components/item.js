2aeecc610a44b336751a9dec53538f2662830ed8
harshLAD <harshk.lad@gmail.com>
2021-09-01 22:15:27 -0400

    Changed item component

--- OrderSummary/order_summary/src/components/item.js
+++ OrderSummary/order_summary/src/components/item.js
@@ -5,14 +5,16 @@ class item extends react.Component {
     return (
       <div className="itmDIV brdr">
         <div className="itmDIV2 w-50 brdr">
-          <div className="w-50 brdr">hh</div>
+          <div className="w-50 brdr">
+            <img src="/public/icon-music.svg" alt="ff"></img>
+          </div>
           <div className="w-50 itmINFO brdr">
             <p>Annual Plan</p>
             <p>$59.99/year</p>
           </div>
         </div>
         <div className="w-50 brdr">
-          <p>Cancel</p>
+          <p>Change</p>
         </div>
       </div>
     );
